41e8c3c2d7b811bb321ebfc924eda6ca32018917
Dave Appleton <calistralabs@gmail.com>
2018-05-12 05:35:08 +0800

    copyright

--- transactions.go
+++ transactions.go
@@ -1,7 +1,7 @@
 package etherscanAPI
 
 /******************************************************************
-Copyright 2018 David Appleton
+Copyright 2017 David Appleton
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
